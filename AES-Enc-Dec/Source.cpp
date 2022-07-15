98bbf049cc759250e0dc4715251d8a73372cdce4
ahmedmoh123 <58375227+ahmedmoh1234@users.noreply.github.com>
2022-07-15 15:19:34 +0200

    Added choices for the user

--- AES-Enc-Dec/Source.cpp
+++ AES-Enc-Dec/Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -8,14 +9,106 @@ const int BUFF_SIZE = 16;
 
 inline string readFileName() ;
 
+inline void encryption();
+inline void decryption();
+
+//What will happen if the file size is smaller than 128 bits ??
+
 int main()
 {
-	//Reading 128 bits from file
-	char buff[BUFF_SIZE];
+	//Ask the user whether they want to encrypt or decrypt
+
+	int encDecChoice;
+	cout << "Enter 1 to ecrypt\n";
+	cout << "Enter 2 to decrypt\n";
+	cin >> encDecChoice;
+	
+	while (encDecChoice != 1 && encDecChoice != 2)
+	{
+		cout << "Wrong choice please enter a correct choice\n";
+		cin >> encDecChoice;
+	}
+
+	if (encDecChoice == 1)
+	{
+		encryption();
+	}
+	else
+	{
+		decryption();
+	}
+
 	
 
-	string fileName = readFileName();
+
+
+	return 0;
+}
+
+inline string readFileName()
+{
+	string fileNameTemp;
+	cout << "Please input filename/path : ";
+	cin >> fileNameTemp;
+	cout << "\n";
+
+	if (fileNameTemp[0] == '"')
+		fileNameTemp.erase(0, 1);
+	if (fileNameTemp[fileNameTemp.size()-1] == '"')
+		fileNameTemp.erase(fileNameTemp.size() - 1, 1);
+
+
+	return std::move(fileNameTemp);
+
+}
+
+inline void encryption()
+{
+	//Ask the user whether to generate a random key or to enter a key
+
+	//The choice of the user
+	int keyChoice;
+
+	//The key that will be used in encryption
+	vector<char> key;
+	cout << "Enter 1 to generate a key randomly\n";
+	cout << "Enter 2 to enter a key manually\n";
+	cin >> keyChoice;
+
+	while (keyChoice != 1 && keyChoice != 2)
+	{
+		cout << "Wrong choice please enter a correct choice\n";
+		cin >> keyChoice;
+	}
+
+	if (keyChoice == 1)
+	{
+		//Random key
+		//Random key will have 3 options
+		//1- 128-bit key
+		//2- 192-bit key
+		//3- 256-bit key
+
+	}
+	else
+	{
+		//Key entered manually
+		//Ask the user to enter the size of the key
+		//128 bits -> 16 characters
+		//192 bits -> 24 characters
+		//256 bits -> 32 characters
+
+		//I did not use a hash function because I want to implement the standart algorithm so any AES decryptor can decrypt it.
+	}
 	
+
+
+	//Reading 128 bits from file
+	char buff[BUFF_SIZE];
+
+
+	string fileName = readFileName();
+
 
 	ifstream inputFile(fileName, ios::binary);
 
@@ -49,27 +142,8 @@ int main()
 			cout << buff[i] << " ";
 		}
 	}
-
-	
-
-
-
-	return 0;
 }
 
-inline string readFileName()
+inline void decryption()
 {
-	string fileNameTemp;
-	cout << "Please input filename/path : ";
-	cin >> fileNameTemp;
-	cout << "\n";
-
-	if (fileNameTemp[0] == '"')
-		fileNameTemp.erase(0, 1);
-	if (fileNameTemp[fileNameTemp.size()-1] == '"')
-		fileNameTemp.erase(fileNameTemp.size() - 1, 1);
-
-
-	return std::move(fileNameTemp);
-
 }
